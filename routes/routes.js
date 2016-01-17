fa188740cae48979bd88df86300c5abe8dfa4d6f
Victor Tang <victortang1992@gmail.com>
2016-01-16 16:27:43 -0800

    time conversions up. technically finished now

--- routes/routes.js
+++ routes/routes.js
@@ -13,14 +13,22 @@ module.exports = function(app) {
 		var reg = /^\d+$/;
 		var json = {}
 		if(reg.test(query)){	//query is all numbers
+			var date = new Date(query*1000);
+			var months = ['January ','February ','March ','April ','May ','June ','July ','August ','September ','October ','November ','December '];
+			var year = date.getFullYear();
+			var month = months[date.getMonth()];
+			var date = date.getDate();
+			// Will display time in 10:30:23 format
+			var natural = month + date + ', ' + year;
 			json = {
-				"unix":query,
-				"natural":"notime"
+				"unix":parseInt(query,10),
+				"natural":natural
 			}
 		}
 		else{						//query is not all numbers
+			var date = new Date(query)/1000;
 			json = {
-				"unix":0,
+				"unix":date,
 				"natural":query
 			}
 		}
